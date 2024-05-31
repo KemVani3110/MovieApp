e4f9a73879b92b05753644e91c639dd0603a3a9a
KemVani3110 <chuminhkhoi3110@gmail.com>
2024-05-31 11:24:48 +0700

    first push

---
+++ screens/UserAccountScreen.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { Text, View, StyleSheet, StatusBar, Image, ScrollView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import AppHeader from '../components/AppHeader';
+import SettingComponent from '../components/SettingComponent';
+
+const UserAccountScreen = ({ navigation }) => {
+  return (
+    <LinearGradient
+      colors={['#4c669f', '#3b5998', '#192f6a']}
+      style={styles.container}
+    >
+      <StatusBar hidden />
+      <View style={styles.appHeaderContainer}>
+        <AppHeader
+          name="close"
+          header="My Profile"
+          action={() => navigation.goBack()}
+        />
+      </View>
+
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.profileContainer}>
+          <Image
+            source={require('../assets/image/avatar.png')}
+            style={styles.avatarImage}
+          />
+          <Text style={styles.avatarText}>Minh Khôi</Text>
+        </View>
+
+        <View style={styles.settingsContainer}>
+          <SettingComponent
+            icon="person"
+            heading="Account"
+            subheading="Edit Profile"
+            subtitle="Change Password"
+          />
+          <SettingComponent
+            icon="settings"
+            heading="Settings"
+            subheading="Theme"
+            subtitle="Permissions"
+          />
+          <SettingComponent
+            icon="attach-money"
+            heading="Offers & Referrals"
+            subheading="Offer"
+            subtitle="Referrals"
+          />
+          <SettingComponent
+            icon="info"
+            heading="About"
+            subheading="About Movies"
+            subtitle="More"
+          />
+        </View>
+      </ScrollView>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  appHeaderContainer: {
+    marginHorizontal: 15,
+    marginTop: 15,
+  },
+  scrollViewContent: {
+    paddingVertical: 30,
+  },
+  profileContainer: {
+    alignItems: 'center',
+    paddingVertical: 30,
+  },
+  avatarImage: {
+    height: 120,
+    width: 120,
+    borderRadius: 60,
+    borderWidth: 2,
+    borderColor: 'white',
+    marginBottom: 20,
+  },
+  avatarText: {
+    fontFamily: 'sans-serif-medium',
+    fontSize: 24,
+    marginTop: 16,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  settingsContainer: {
+    padding: 15,
+    marginTop: 20,
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    borderRadius: 20,
+  },
+});
+
+export default UserAccountScreen;
